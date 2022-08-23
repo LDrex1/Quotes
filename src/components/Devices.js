99678d892a55b68f72a339839244aaec35ef2553
lDrex1 <damzealwini7@gmail.com>
2022-08-23 13:12:47 +0100

    device width

---
+++ src/components/Devices.js
@@ -0,0 +1,17 @@
+//
+const size = {
+  mobileS: "380px",
+  mobileM: "548px",
+  mobileL: "769px",
+  laptop: "1023px",
+};
+//
+
+const device = {
+  mobileS: `(min-width: ${size.mobileS})`,
+  mobileM: `(min-width: ${size.mobileM})`,
+  mobileL: `(min-width: ${size.mobileL})`,
+  laptop: `(min-width: ${size.laptop})`,
+};
+
+export default device;
