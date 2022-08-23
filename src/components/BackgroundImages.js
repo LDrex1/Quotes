0d4950ddc4f63fd0916141edcf2384f558b5b558
lDrex1 <damzealwini7@gmail.com>
2022-08-23 15:26:09 +0100

    new component for background images

---
+++ src/components/BackgroundImages.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BackgroundImages = {
+  signIn: [
+    require("../images/greek_philosophers.jpg"),
+    require("../images/image_1.jpg"),
+    require("../images/image_2.jpg"),
+  ],
+};
+
+export default BackgroundImages;
