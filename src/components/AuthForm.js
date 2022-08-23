649d76b8230abb4e617b2e1464fa9a33cbaa9b8d
lDrex1 <damzealwini7@gmail.com>
2022-08-23 13:13:45 +0100

    update: form style and second button style

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -1,36 +1,38 @@
 import React from "react";
 import { useState, useRef } from "react";
 import styled from "styled-components";
+import device from "./Devices";
 
 function AuthForm(props) {
   const usernameRef = useRef();
   const emailRef = useRef();
   const passwordRef = useRef();
   const confirmPasswordRef = useRef();
-
+  console.log(props.formType);
   const usernamePattern = /\b[a-z_][a-z_]+\d*\b|\b[a-z_]\d\d+\b/i;
-  const passwordPattern = /(=?.*[a-z])(=?.*\d)(?=.{4}).*|(=?\d)(=?[a-z])/i;
-
+  const passwordPattern =
+    /(?=^.{6,9}$)(=?.*[A-Z])(=?.*\d)|(?=^.{6,9}$)(=?.*\d)(=?.*[A-Z])/;
   const onSubmit = (ev) => {
     ev.preventDefault();
     console.log(emailRef.current.value);
   };
+  console.log(passwordRef.current);
   return (
     <>
-      <Form onSubmit={onSubmit}>
+      <Form onSubmit={onSubmit} formType={props.formType}>
         <InputDiv style={props.usernameInputDisplay}>
           <Input
             ref={usernameRef}
             type={"text"}
             pattern={usernamePattern}
             placeholder={"Username"}
-            required
+            required={props.usernameRequired}
           ></Input>
         </InputDiv>
-        <InputDiv>
+        <InputDiv className="">
           <Input
             ref={emailRef}
-            type={"text"}
+            type={"email"}
             inputMode={"email"}
             placeholder={"Email"}
             required
@@ -49,14 +51,18 @@ function AuthForm(props) {
           <Input
             ref={confirmPasswordRef}
             type={"password"}
-            pattern={/(=?[a-z])(=?\d)|(=?\d)(=?[a-z])/i}
             placeholder={"Confirm Password"}
-            required
+            required={props.confirmPasswordRequired}
           ></Input>
         </InputDiv>
-        <ButtonsDiv>
+        <ButtonsDiv className="">
           <Button>{props.firstButton}</Button>
-          <Button>{props.secondButton}</Button>
+          <Button className="second">
+            <div className="google-logo">
+              <img src={require("../images/google_logo.jpg")}></img>
+            </div>
+            <div>{props.secondButton}</div>
+          </Button>
         </ButtonsDiv>
       </Form>
     </>
@@ -65,15 +71,66 @@ function AuthForm(props) {
 
 export default AuthForm;
 
-const Form = styled.form``;
+const Form = styled.form`
+  width: 70%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  row-gap: ${(props) => (props.formType == "sign-in" ? "25px" : "14px")};
+`;
 
-const InputDiv = styled.div``;
+const InputDiv = styled.div`
+  text-align: center;
+`;
 
-const Input = styled.input``;
+const Input = styled.input`
+  width: 80%;
+  &:focus {
+    border: 3px #144f7c;
+    box-shadow: 0.4px 0px 1.2px 1px #1a659d;
+  }
+  @media ${device.mobileL} {
+    background: black;
+    width: 80%;
+  }
+`;
 
 const ButtonsDiv = styled.div`
   display: flex;
-  column-gap: 10px;
+  justify-content: center;
+  column-gap: 15px;
 `;
 
-const Button = styled.button``;
+const Button = styled.button`
+  &.second {
+    padding: 0;
+    background: #4285f4;
+    display: flex;
+    align-items: center;
+    color: whitesmoke;
+    min-width: 160px;
+      div {
+        padding 8px 0px;
+        text-align: center;
+      }
+      div.google-logo {
+        margin-left: 0.7px;
+        margin-right: 6px;
+        padding: 1.2px;
+        background: #ffffff;
+        display: flex;
+        align-items: center;
+        width: 26px;
+        height: 95%;
+        aspect-ratio: 0.9;
+      }
+      
+      img {
+        width: 100%;
+        object-fill: fill;
+        height: 80%;
+      }
+  }
+`;
+
+const Container = styled.div``;
