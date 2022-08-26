ac0f564fa1e5a3ab141870fad97ed7654d2144d9
lDrex1 <damzealwini7@gmail.com>
2022-08-26 20:05:11 +0100

    update: Dark mode toggler

--- src/components/QuotesPage.js
+++ src/components/QuotesPage.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import FooterG from "./Footer";
+import { useTheme } from "./ThemeProvider";
 import QuotesPageHeader from "./QuotesPageHeader";
 import Quotes from "./Quotes";
 
 function QuotesPage() {
+  const theme = useTheme()[0];
+  const toggleTheme = useTheme()[1];
+
   return (
-    <Wrapper>
+    <Wrapper theme={theme}>
+      <Button onClick={toggleTheme} theme={theme}>
+        {theme ? "DarkMode" : "LightMode"}
+      </Button>
       <QuotesPageHeader />
       <Main>
         <Quotes />
@@ -21,8 +28,23 @@ export default QuotesPage;
 const Wrapper = styled.div`
   min-height: 100vh;
   position: relative;
+  background: ${(props) => (props.theme === true ? "#ffffff" : "#222222")};
+  color: ${(props) => (props.theme === true ? "black" : "whitesmoke")};
+  button {
+    background: ${(props) => (props.theme === true ? "#ffffff" : "#222222")};
+    color: ${(props) => (props.theme === true ? "black" : "whitesmoke")};
+  }
 `;
 
-const Header = styled.header``;
+const Button = styled.button`
+  background: ${(props) =>
+    !(props.theme === true) ? "#ffffff" : "#222222"} !important;
+  color: ${(props) =>
+    !(props.theme === true) ? "black" : "whitesmoke"} !important;
+  position: absolute;
+  bottom: 90px;
+  right: 5%;
+  width: fit-content;
+`;
 
 const Main = styled.main``;
