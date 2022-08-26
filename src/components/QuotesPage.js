7b11fa694ead0e422570d20b83d156e410749a6b
lDrex1 <damzealwini7@gmail.com>
2022-08-26 15:55:20 +0100

    Update: "Quotes" component and "Wrapper" style

--- src/components/QuotesPage.js
+++ src/components/QuotesPage.js
@@ -1,23 +1,28 @@
 import React from "react";
 import styled from "styled-components";
+import FooterG from "./Footer";
 import QuotesPageHeader from "./QuotesPageHeader";
+import Quotes from "./Quotes";
 
 function QuotesPage() {
   return (
     <Wrapper>
       <QuotesPageHeader />
-      <Main></Main>
-      <Footer></Footer>
+      <Main>
+        <Quotes />
+      </Main>
+      <FooterG />
     </Wrapper>
   );
 }
 
 export default QuotesPage;
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  min-height: 100vh;
+  position: relative;
+`;
 
 const Header = styled.header``;
 
 const Main = styled.main``;
-
-const Footer = styled.footer``;
