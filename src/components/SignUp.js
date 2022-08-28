74d5a822d48069550a1b72dd98266cda80b8b72e
lDrex1 <damzealwini7@gmail.com>
2022-08-28 23:30:40 +0100

    update: fromValues state and createUser function

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,12 +1,25 @@
 import React from "react";
-import { useState, useRef } from "react";
+import { useState } from "react";
 import styled from "styled-components";
-import { auth } from "./../firebase-config";
+import { createUserWithEmailAndPassword } from "@firebase/auth";
+import { auth, db } from "./../firebase-config";
 import AuthForm from "./AuthForm";
 
 function SignUp() {
+  const [formValues, updateFormValues] = useState({});
+  const { username, email, password, confirmPassword } = formValues;
   const usernameInputDisplay = {};
   const confirmPasswordInputDisplay = {};
+
+  const createUser = async (ev) => {
+    ev.preventDefault();
+    let userUid = await createUserWithEmailAndPassword(auth, email, password)
+      .then((userCred) => {
+        const { user } = userCred;
+        return user.uid;
+      })
+      .catch((err) => console.log(err.code, err.line));
+  };
   return (
     <>
       <Wrapper>
@@ -15,6 +28,8 @@ function SignUp() {
           confirmPasswordInputDisplay={confirmPasswordInputDisplay}
           firstButton={"Sign Up"}
           secondButton={"Sign Up with Google"}
+          formValues={formValues}
+          updateFormValues={updateFormValues}
         />
       </Wrapper>
     </>
