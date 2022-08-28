b024201b54d4f7f23d1d50249e86e809b4246fbc
lDrex1 <damzealwini7@gmail.com>
2022-08-28 23:27:52 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
+import React from "react";
+import { useState } from "react";
 import "./App.css";
 import SignUp from "./components/SignUp";
 import SignIn from "./components/SignIn";
 import ThemeProvider from "./components/ThemeProvider";
+import { FormValuesContext, useFormValues } from "./components/AuthForm";
 import QuotesPage from "./components/QuotesPage";
 
 function App() {
+  // console.log(useFormValues());
   return (
     <>
-      {/* <SignUp /> */}
+      <SignUp />
+
       {/* <SignIn /> */}
-      <ThemeProvider>
+      {/* <ThemeProvider>
         <QuotesPage />
-      </ThemeProvider>
+      </ThemeProvider> */}
     </>
   );
 }
