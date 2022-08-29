4c79918a070030516b6fa9ea8887c6c0ceb92d57
lDrex1 <damzealwini7@gmail.com>
2022-08-29 23:42:25 +0100

    update: theme

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,11 +1,17 @@
 import React from "react";
 import styled from "styled-components";
+import { useTheme } from "./ThemeProvider";
 
 function Quotes() {
+  const [theme, toggleTheme] = useTheme();
+
   return (
     <>
-      <QuotesDiv>
-        <P>Never Give up</P>
+      <QuotesDiv theme={theme}>
+        <P>
+          <q>Never Give up</q>
+        </P>
+        <H5>demo</H5>
       </QuotesDiv>
     </>
   );
@@ -15,11 +21,17 @@ export default Quotes;
 
 const QuotesDiv = styled.div`
   border: 1px solid red;
+  background: ${(props) => (props.theme === true ? "#f4f7f5" : "#232723")};
   width: 80%;
+  padding: 8px;
   height: 55px;
   margin: auto;
 `;
 
 const P = styled.div`
   padding: 4px 5px;
+`;
+
+const H5 = styled.h5`
+  text-align: end;
 `;
