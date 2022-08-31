0d67c5640f5a299e1f2f23f5025dd78e125bfd74
lDrex1 <damzealwini7@gmail.com>
2022-08-31 13:49:28 +0100

    useTheme imported and signOut fuction

--- src/components/QuotesPageHeader.js
+++ src/components/QuotesPageHeader.js
@@ -1,8 +1,21 @@
 import React from "react";
+import { useContext } from "react";
 import styled from "styled-components";
+import { signOut } from "@firebase/auth";
+import { auth } from "./../firebase-config";
 import device from "./Devices";
+import { useTheme } from "./ThemeProvider";
 
 function QuotesPageHeader() {
+  const [theme] = useTheme();
+  const signOut = (ev) => {
+    ev.preventDefault();
+    (async () => {
+      let signedOut = await signOut(auth);
+      signedOut.then((resp) => console.log(resp, "out"));
+    })();
+  };
+
   return (
     <>
       <Header>
@@ -14,12 +27,12 @@ function QuotesPageHeader() {
             <ImageDiv>
               Account<img src=""></img>
             </ImageDiv>
-            <Ul className="dropdown-content">
+            <Ul className="dropdown-content" theme={theme}>
               <Li>
                 <A>Edit profile</A>
               </Li>
               <Li>
-                <Button>Log Out</Button>
+                <Button onClick={signOut}>Log Out</Button>
               </Li>
             </Ul>
           </Div>
@@ -70,20 +83,28 @@ const Div = styled.div`
 const ImageDiv = styled.div``;
 
 const Ul = styled.ul`
-  border: 2px solid red;
+  border: 1px solid #ffffff;
+  border-radius: 3px;
+  box-shadow: -0.3px 0px 4px #222222;
+  background: ${(props) => (props.theme === true ? "#ffffff" : "#222222")};
   padding: 16px 0 16px 0;
-  max-height: 0;
+  max-height: 0px;
   display: flex;
   flex-direction: column;
   row-gap: 10px;
   text-align: center;
   position: absolute;
-  top: 20px;
+  top: 24px;
   width: 100%;
 `;
 
-const Li = styled.li``;
+const Li = styled.li`
+  color: #c1b02d;
+`;
 
 const A = styled.a``;
 
-const Button = styled.button``;
+const Button = styled.button`
+  background: red !important;
+  color: red;
+`;
