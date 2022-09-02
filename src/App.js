2ae111dea62c6842d60cd9706817e3806c51627e
lDrex1 <damzealwini7@gmail.com>
2022-09-02 23:06:28 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from "react";
-import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import SignUp from "./components/SignUp";
 import SignIn from "./components/SignIn";
 import ThemeProvider from "./components/ThemeProvider";
-import { FormValuesContext, useFormValues } from "./components/AuthForm";
 import QuotesPage from "./components/QuotesPage";
 
 function App() {
-  // console.log(useFormValues());
   return (
     <>
       <Routes>
