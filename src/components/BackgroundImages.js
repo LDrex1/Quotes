7e9b974d055bd41c7e21522109d873c633348135
lDrex1 <damzealwini7@gmail.com>
2022-09-03 20:17:37 +0100

    added: sign-up background files

--- src/components/BackgroundImages.js
+++ src/components/BackgroundImages.js
@@ -3,6 +3,13 @@ const BackgroundImages = {
     require("../images/plato.jpg"),
     require("../images/image_1.jpg"),
     require("../images/image_2.jpg"),
+    require("../images/image_4.jpg"),
+  ],
+  signUp: [
+    require("../images/image-sign-up-1.jpg"),
+    require("../images/image-sign-up-2.jpg"),
+    require("../images/image-sign-up-3.jpg"),
+    require("../images/image-sign-up-4.jpg"),
   ],
 };
 
